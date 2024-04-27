392d4499aaf053027f803853842f295b64872184
zenthic22 <roberchac75@gmail.com>
2024-04-27 00:14:47 -0600

    corregido

--- CargaDatos.sql
+++ CargaDatos.sql
@@ -1,7 +1,24 @@
-
-
-
 -- Crear una tabla temporal en MySQL para cargar los datos del archivo Excel
+-- tabla etnia
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_etnia(
+    columna1 INT,
+    columna2 VARCHAR(240),
+    columna3 VARCHAR(240)
+    
+);
+
+LOAD DATA INFILE 'C:/etnias.csv'
+INTO TABLE temp_etnia
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+INSERT INTO etnia (id_etnia, sexo, raza)
+SELECT columna1, columna2, columna3
+FROM temp_etnia;
+
+DROP TEMPORARY TABLE IF EXISTS temp_etnia;
 
 
 CREATE TEMPORARY TABLE IF NOT EXISTS temp_caracteristica (
@@ -16,7 +33,7 @@ CREATE TEMPORARY TABLE IF NOT EXISTS temp_caracteristica (
 );
 
 -- Cargar datos desde el archivo Excel a la tabla temporal
-LOAD DATA INFILE 'ruta'
+LOAD DATA INFILE 'C:/caracteristicas.csv'
 INTO TABLE temp_caracteristica
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -26,7 +43,7 @@ IGNORE 1 ROWS;
 -- Si necesitas transformar o limpiar los datos antes de insertarlos en la tabla final, puedes hacerlo aquí
 
 -- Insertar datos desde la tabla temporal a la tabla final
-INSERT INTO caracteristica (id_tipo, analfabetos, alfabetos, primaria, nivel_medio, universitarios, etnia_id_etnia)
+INSERT INTO caracteristica (id_tipo, id_etnia, analfabetos, alfabetos, primaria, nivel_medio, universitarios)
 SELECT columna1, columna2, columna3, columna4, columna5, columna6, columna7
 FROM temp_caracteristica;
 
@@ -39,12 +56,12 @@ DROP TEMPORARY TABLE IF EXISTS temp_caracteristica;
 CREATE TEMPORARY TABLE IF NOT EXISTS temp_eleccion(
     columna1 INT,
     columna2 VARCHAR(240),
-    columna3 DATE,
+    columna3 VARCHAR(240),
     columna4 VARCHAR(240),
     columna5 VARCHAR(240)
 );
 
-LOAD DATA INFILE 'RUTA'
+LOAD DATA INFILE 'C:/elecciones.csv'
 INTO TABLE temp_eleccion
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -57,52 +74,26 @@ FROM temp_eleccion;
 
 DROP TEMPORARY TABLE IF EXISTS temp_eleccion;
 
--- tabla etnia
-CREATE TEMPORARY TABLE IF NOT EXISTS temp_etnia(
-    columna1 INT,
-    columna2 VARCHAR(240),
-    columna3 VARCHAR(240)
-    
-);
-
-LOAD DATA INFILE 'RUTA'
-INTO TABLE temp_etnia
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 ROWS;
-
-INSERT INTO etnia (id_etnia, sexo, raza)
-SELECT columna1, columna2, columna3
-FROM temp_etnia;
-
-DROP TEMPORARY TABLE IF EXISTS temp_etnia;
-
 -- tabla lugares
 CREATE TEMPORARY TABLE IF NOT EXISTS temp_lugares(
     columna1 INT,
-    columna2 VARCHAR(240),
+    columna2 INT,
     columna3 VARCHAR(240),
     columna4 VARCHAR(240),
     columna5 VARCHAR(240),
-    columna6 INT,
+    columna6 VARCHAR(240),
     columna7 INT
-    
 );
 
-LOAD DATA INFILE 'RUTA'
+LOAD DATA INFILE 'C:/lugares.csv'
 INTO TABLE temp_lugares
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-INSERT INTO lugares (id_lugar, pais, region, depto, municipio, eleccion_id_eleccion, caracteristica_id_tipo)
+INSERT INTO lugares (id_lugar, id_eleccion, pais, region, depto, municipio, id_caracteristica)
 SELECT columna1, columna2, columna3, columna4, columna5, columna6, columna7
-FROM temp_etnia;
+FROM temp_lugares;
 
 DROP TEMPORARY TABLE IF EXISTS temp_lugares;
-
-
-
-
--- ScriptCreacion.sql
+++ ScriptCreacion.sql
@@ -1,23 +1,18 @@
--- Generado por Oracle SQL Developer Data Modeler 23.1.0.087.0806
---   en:        2024-04-26 18:43:06 CST
---   sitio:      Oracle Database 11g
---   tipo:      Oracle Database 11g
-
 CREATE TABLE caracteristica (
     id_tipo        INT NOT NULL,
+    id_etnia INT NOT NULL,
     analfabetos    INT NOT NULL,
     alfabetos      INT NOT NULL,
     primaria       INT NOT NULL,
     nivel_medio    INT NOT NULL,
     universitarios INT NOT NULL,
-    etnia_id_etnia INT NOT NULL,
     PRIMARY KEY (id_tipo)
 );
 
 CREATE TABLE eleccion (
     id_eleccion     INT NOT NULL,
     nombre_eleccion VARCHAR(240) NOT NULL,
-    anio_eleccion   DATE NOT NULL,
+    anio_eleccion   INT NOT NULL,
     partido         VARCHAR(240) NOT NULL,
     nombre_partido  VARCHAR(240) NOT NULL,
     PRIMARY KEY (id_eleccion)
@@ -32,23 +27,23 @@ CREATE TABLE etnia (
 
 CREATE TABLE lugares (
     id_lugar               INT NOT NULL,
+    id_eleccion   INT NOT NULL,
     pais                   VARCHAR(240) NOT NULL,
     region                 VARCHAR(240) NOT NULL,
     depto                  VARCHAR(240) NOT NULL,
     municipio              VARCHAR(240) NOT NULL,
-    eleccion_id_eleccion   INT NOT NULL,
-    caracteristica_id_tipo INT NOT NULL,
+    id_caracteristica INT NOT NULL,
     PRIMARY KEY (id_lugar)
 );
 
 ALTER TABLE caracteristica
-    ADD CONSTRAINT caracteristica_etnia_fk FOREIGN KEY (etnia_id_etnia)
+    ADD CONSTRAINT caracteristica_etnia_fk FOREIGN KEY (id_etnia)
         REFERENCES etnia (id_etnia);
 
 ALTER TABLE lugares
-    ADD CONSTRAINT lugares_caracteristica_fk FOREIGN KEY (caracteristica_id_tipo)
+    ADD CONSTRAINT lugares_caracteristica_fk FOREIGN KEY (id_caracteristica)
         REFERENCES caracteristica (id_tipo);
 
 ALTER TABLE lugares
-    ADD CONSTRAINT lugares_eleccion_fk FOREIGN KEY (eleccion_id_eleccion)
+    ADD CONSTRAINT lugares_eleccion_fk FOREIGN KEY (id_eleccion)
         REFERENCES eleccion (id_eleccion);
